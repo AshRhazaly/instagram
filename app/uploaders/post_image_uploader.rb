85802ce873a2a4257fa4911f0b82165d3dc7dc1b
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-03-21 22:51:05 +0800

    Add grid system

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -31,6 +31,10 @@ class PostImageUploader < CarrierWave::Uploader::Base
     process resize_to_fit: [350, 350]
   end
 
+  version :large do
+    process resize_to_fit: [800,800]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_whitelist
